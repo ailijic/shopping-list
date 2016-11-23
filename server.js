ba3e35f77b8c76e76a6061c0e18b0d038c2d33b3
Andrew Ilijic <ailijic@gmail.com>
2016-11-23 13:38:32 -0500

    Modify put method

--- server.js
+++ server.js
@@ -22,6 +22,26 @@ function start() {
     this.items.splice(deleteMe, 1);
   };
 
+  //Extend Storage object for PUT items
+  storage.put = function (item) {
+    const getIndex = function getIndex(id) {
+      const equalToId = function equalToId(value, index, array) {
+        return (value.id === id);
+      }
+      return this.items.findIndex(equalToId);
+    }
+
+    const indexNotFound = -1;
+    if (getIndex(item.id) === indexNotFound) {
+      // add new record
+      let newItem = this.add(item.name);
+      newItem.id = item.id;
+    } else {
+      this.items[getIndex(item.id)].name = item.name;
+    }
+  }
+
+
   // CRUD Handlers
   // // GET defined in rest_module.js
   // // POST defined in rest_module.js
@@ -61,9 +81,66 @@ function start() {
   //   - Strip out everything in the request except for .id and .name 
   //   - Params ID must match the ID in the body object (===)
   app.put('/items/:id', jsonParser, (request, response) => {
-    // sanitize the input and return clean object
-        let Sanitize = () => {return {};};
-    const messageObj = Sanitize()
+    let putObj = {
+      status: undefined,
+      retMsg: "",
+      record: {}, // {id: id, name: String},
+    };
+    
+
+    // Sanitize the input and return object with only Name & ID
+    putObj = sanitize(request);
+
+    putObj = (function setStatus() {
+      putObj.status = 200;
+      put retMsg = putObj.record;
+      return putObj;
+    }) ();
+
+    // UpdateDB
+    if (putObj.status === 200) {
+      storage.put(putObj.record);
+    }
+    
+    // return response
+    return response.status(putObj.status).json(putObj.retMsg);
+
+    function sanitize(request) {
+      const name = request.body.name;
+      const id = request.body.id;
+      switch (true) {
+        case (name === undefined):
+          console.log("ERR: Name undefined");
+          putObj.status = 404;
+          const retString = "Message body missing name property";
+          putObj.retMsg = {message: retString};
+          break;
+        case (typeof(name) !== 'string'):
+          console.log("ERR: Name is not a string");
+          putObj.status = 404;
+          const retString = "Name is not a string";
+          putObj.retMsg = {message: retString};
+          break;
+        case (id === undefined):
+          console.log("ERR: ID is undefined");
+          putObj.status = 404;
+          const retString = "Message body ID is not defined";
+          putObj.retMsg = {message: retString};
+          break;
+        case (typeof(id) !== "number"):
+          console.log("ERR: ID is not a number");
+          putObj.status = 404;
+          const retString = "ID is not a number";
+          putObj.retMsg = {message: retString};
+          break;
+        default:
+          putObj.record.name = name;
+          putObj.record.id = id;
+      }
+      return putObj;
+    }
+  });
+
     const id = parseInt(request.params.id);
     const messageBody = request.body;
     isMessageBodyMalformed(messageBody);
@@ -73,10 +150,10 @@ function start() {
     }
     if (idExists(id)) {
       const IndexVal = storage.items.findIndex((valObj) => {
-      if (valObj.id === id) {
-        return true;
-      }
-    });
+        if (valObj.id === id) {
+          return true;
+        }
+      });
       const record = storage.items[IndexVal];
       record.name = messageBody.name;
       return response.status(200).json(record);
